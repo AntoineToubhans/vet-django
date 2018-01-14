a703c93348e2e3dfb68bb7c2afa686c5ad52176d
Antoine Toubhans <antoine@toubhans.org>
2018-01-14 17:18:53 +0100

    Add current page

--- vet_app/views.py
+++ vet_app/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home.html', {
         'menu': settings.VET_APP_MENU,
+        'current_page': 'home',
     })
 
 
 def team(request):
     return render(request, 'team.html', {
         'menu': settings.VET_APP_MENU,
+        'current_page': 'team',
     })
