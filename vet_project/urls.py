825cb011db6b8b75de213c16b490f6b65506b5a1
Antoine Toubhans <antoine@toubhans.org>
2018-01-15 19:01:32 +0100

    Add static path to urls

--- vet_project/urls.py
+++ vet_project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -22,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vet_app.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
