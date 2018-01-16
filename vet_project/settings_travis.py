82a8ec79be88f8831a5abbaf409d9adc6a4e650c
Antoine Toubhans <antoine@toubhans.org>
2018-01-16 21:25:52 +0100

    Add travis CI

---
+++ vet_project/settings_travis.py
@@ -0,0 +1,13 @@
+from vet_project.settings import *
+
+
+DATABASES = {
+ 'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+     'NAME': 'postgres',
+     'USER': 'postgres',
+     'PASSWORD': 'postgres',
+     'HOST': 'localhost',
+     'PORT': 5432,
+ }
+}
