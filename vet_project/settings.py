0465f1c769b0631d6dd793f8fb774db416ea4b65
Antoine Toubhans <antoine@toubhans.org>
2018-01-19 07:48:08 +0100

    Add custom ckeditor config

--- vet_project/settings.py
+++ vet_project/settings.py
@@ -160,6 +160,75 @@ STATICFILES_FINDERS = [
 ]
 
 
+# CKeditor config
+CKEDITOR_IMAGE_BACKEND = 'pillow'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar_Basic': [
+            ['Source', '-', 'Bold', 'Italic']
+        ],
+        'toolbar_VetToolbarConfig': [{
+            'name': 'document',
+            'items': ['Source', '-', 'NewPage', 'Preview', 'Print', '-', 'Templates'],
+        }, {
+            'name': 'clipboard',
+            'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'],
+        }, {
+            'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll'],
+        }, {
+            'name': 'forms',
+            'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        }, '/', {
+            'name': 'basicstyles',
+            'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
+        }, {
+            'name': 'paragraph',
+            'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+                      'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
+                      'Language'],
+        }, {
+            'name': 'links',
+            'items': ['Link', 'Unlink', 'Anchor'],
+        }, {
+            'name': 'insert',
+            'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'],
+        }, '/', {
+            'name': 'styles',
+            'items': ['Styles', 'Format', 'Font', 'FontSize'],
+        }, {
+            'name': 'colors',
+            'items': ['TextColor', 'BGColor'],
+        }, {
+            'name': 'tools',
+            'items': ['Maximize', 'ShowBlocks'],
+        }, {
+            'name': 'about',
+            'items': ['About', 'Save'],
+        }],
+        'toolbar': 'VetToolbarConfig',
+        'width': '100%',
+        'tabSpaces': 4,
+        'extraPlugins': ','.join([
+            'uploadimage',
+            # your extra plugins here
+            'div',
+            'autolink',
+            'autoembed',
+            'embedsemantic',
+            'autogrow',
+            'language',
+            # 'devtools',
+            'widget',
+            'lineutils',
+            'clipboard',
+            'dialog',
+            'dialogui',
+            'elementspath'
+        ]),
+    }
+}
+
+
 # Vet app specific options
 VET_APP_MENU = [{
     'href': 'advises',
